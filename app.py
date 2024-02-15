a1a81830b184900d9ca9f405ea4bd32eead82e29
Thomas Martin <tmartin@krakensubsea.com>
2024-02-15 11:10:29 +0100

    menu test

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit_antd_components as sac
+
 
 st.set_page_config(
     page_title="Hello",
@@ -8,6 +10,24 @@ st.set_page_config(
 st.write("# Welcome to Streamlit! 👋")
 
 
+
+sac.menu([
+    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+    sac.MenuItem('products', icon='box-fill', children=[
+        sac.MenuItem('apple', icon='apple'),
+        sac.MenuItem('other', icon='git', description='other items', children=[
+            sac.MenuItem('google', icon='google', description='item description'),
+            sac.MenuItem('gitlab', icon='gitlab'),
+            sac.MenuItem('wechat', icon='wechat'),
+        ]),
+    ]),
+    sac.MenuItem('disabled', disabled=True),
+    sac.MenuItem(type='divider'),
+    sac.MenuItem('link', type='group', children=[
+        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+    ]),
+], open_all=True)
 
 st.markdown(
     """
